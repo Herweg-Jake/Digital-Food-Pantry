e53eff2874fbaec58cfc37645ace2cb3218016b0
David-Rino <rinodavid@nevada.unr.edu>
2024-04-13 16:21:23 -0700

    Added Create User

--- Database.py
+++ Database.py
@@ -68,6 +68,11 @@ class Database():
             return None
 
     def returnPantry(self, username):
+
+        exists = self.food.find_one({"username": username})
+        if not exists:
+            self.createUser(username)
+
         result = self.food.find_one(
             {"username": username}, {"_id": 0, "foods": 1}
         )
@@ -77,6 +82,15 @@ class Database():
         else:
             print(f"No items in pantry found for {username}")
             return {}
+
+    def createUser(self, username):
+
+        exists = self.food.find_one({"username": username})
+        if not exists:
+            self.food.insert_one({"username": username, "foods": {}})
+            print(f"New user {username} created with an empty ")
+        else:
+            print(f"User {username} already exists")
 
 if __name__ == "__main__":
     foodPantry = Database()
@@ -95,17 +109,29 @@ if __name__ == "__main__":
     fdc_id = fdcid_find(val, 1)
     nutrients = fetch_nutrients(fdc_id)
 
-    foodPantry.addFood(username = "Joe Mama", name = "Hot Dogs", purchaseCost = 1.00, currentServing = 5, intialServingCount = 10, purchaseDate = "2024-04-13", expireDate = "2024-04-20", macros = nutrients)
+    foodPantry.addFood(username = "Joe Fatha", name = "Hot Dogs", purchaseCost = 1.00, currentServing = 5, intialServingCount = 10, purchaseDate = "2024-04-13", expireDate = "2024-04-20", macros = nutrients)
     #foodPantry.remove_food("Rino", "Joe Beans")
     #foodPantry.editFood("Rino", "Joe Fruits", 8)
     #print(foodPantry.getFood("Rino", "Joe Beans"))
-    pantry = foodPantry.returnPantry("Joe Mama")
+    pantry = foodPantry.returnPantry("Joe Fatha")
     print(pantry)
     if pantry:
         for foodKey, details in pantry.items():
             print(f"{foodKey.replace('_', ' ')}: {details}")
     else:
         print("Nothing to display")
+
+    if pantry:
+        print(f"All the servings in pantry:")
+        for foodKey, details in pantry.items():
+            currentServing = details.get('currentServing', 'None')
+            many = len(pantry)
+            print(many)
+
+            foodName = foodKey.replace('_', ' ')
+            print(f"{foodName}: Current serving = {currentServing}")
+    else:
+        print("You ate everything")
 
 
 
